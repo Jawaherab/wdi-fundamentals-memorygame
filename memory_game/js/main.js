a5437c0266b81bef6fc7ce665bfcce520d14f85d
Jawaher Abdullah <jawaher@MacBook-Pro-alkhas-b-Jawaher.local>
2019-04-18 02:25:04 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -8,3 +8,21 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTow);
 console.log("User flipped " + cardThree);
 console.log("User flipped " + cardFour);
+
+var cards = new Array();
+var cards = ["queen" , "queen" , "King" , "King"];
+var cardsInPlay = [0];
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTow');
+
+
+	if (cardOne === cardTow) {
+		alert("You found a match!");
+
+	} else {
+		alert("sorry , try again");
+	}
+
+
+
