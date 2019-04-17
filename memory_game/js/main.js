6993d7876d3b454801b7182e5d0766052e337ef9
Jawaher Abdullah <jawaher@MacBook-Pro-alkhas-b-Jawaher.local>
2019-04-18 00:51:02 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTow = "King";
+var cardThree = "joker";
+var cardFour = "princess";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTow);
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
