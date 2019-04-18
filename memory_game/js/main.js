d0a88190a0e1b0267a4fa22b95fe3424efdbb6d4
Jawaher Abdullah <jawaher@MacBook-Pro-alkhas-b-Jawaher.local>
2019-04-18 04:12:53 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,25 +4,39 @@ var cardTow = "King";
 var cardThree = "joker";
 var cardFour = "princess";
 
+var cards = new Array( "queen" , "king" );
+var cardsInPlay [];
+
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTow);
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);
 
-var cards = new Array();
-var cards = ["queen" , "queen" , "King" , "King"];
-var cardsInPlay = [0];
-
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTow');
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
 
 
-	if (cardOne === cardTow) {
-		alert("You found a match!");
 
-	} else {
-		alert("sorry , try again");
-	}
+  function flipCard (cardId) {
+    
+    console.log("User flipped" + cards[cardId] );
+  }
+
+ flipCard (0);
+ flipCard (2);
+
+  function checkForMatch() {
+
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+  }
+
+  
 
 
 
